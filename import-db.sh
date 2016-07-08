d967e6ad9c7e3330f52d23561203d925f4513b35
Eric Jinks <ericjinks@gmail.com>
2016-07-08 12:08:29 +1000

    Add import-db script for importing existing WP sites

---
+++ import-db.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+BRN='\033[0;33m' # headline
+
+printf "${BRN}========== IMPORTING WP DB ==========${NC}\n\n"
+
+vagrant ssh -c "
+printf '»»» checking wp cli version\n' &&
+sudo wp cli update --allow-root &&
+cd /var/www/public &&
+printf '»»» checking db credentials\n' &&
+sed -i \"s/'DB_USER', 'wp'/'DB_USER', 'root'/g\" wp-config.php
+sed -i \"s/'DB_PASSWORD', 'wp'/'DB_USER', 'root'/g\" wp-config.php
+printf '»»» creating db\n' &&
+wp db create &&
+printf '»»» importing db\n' &&
+wp db import"
+
+printf "\n${BRN}========== IMPORTING WP DB FINISHED ==========${NC}\n"
