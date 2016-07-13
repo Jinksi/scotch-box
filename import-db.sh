924b07ee97ca6546657bb69dede761c49a7a00ba
Eric Jinks <ericjinks@gmail.com>
2016-07-13 10:50:00 +1000

    Fix search-replace

--- import-db.sh
+++ import-db.sh
@@ -11,7 +11,7 @@ sudo wp cli update --allow-root &&
 cd /var/www/public &&
 printf '»»» checking db credentials\n' &&
 sed -i \"s/'DB_USER', 'wp'/'DB_USER', 'root'/g\" wp-config.php
-sed -i \"s/'DB_PASSWORD', 'wp'/'DB_USER', 'root'/g\" wp-config.php
+sed -i \"s/'DB_PASSWORD', 'wp'/'DB_PASSWORD', 'root'/g\" wp-config.php
 printf '»»» creating db\n' &&
 wp db create &&
 printf '»»» importing db\n' &&
